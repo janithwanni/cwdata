367378ff3de2a68b4a987ea96c85e5644e355070
janithwanni <janithcwanni@gmail.com>
2024-10-04 13:46:33 +1000

    add and document new function

--- R/run-app.R
+++ R/run-app.R
@@ -3,3 +3,17 @@ run_app <- function() {
   app_dir <- system.file("eda-app", package = "cwdata")
   shiny::runApp(app_dir, display.mode = "normal")
 }
+
+#' @title title
+#' @description
+#' A short description...
+#' @returns Nothing
+#' @examples
+#' \dontrun{
+#'   run_my_app()
+#' }
+#' @export
+run_my_app <- function() {
+  app_dir <- system.file("my-app", package = "cwdata")
+  shiny::runApp(app_dir, display.mode = "normal")
+}
